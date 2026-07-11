4542328cf0b54ecc8abcfdc230bc0d2c4e2ec8db
tsunat973 <t23am018@yamanashi.ac.jp>
2026-07-11 20:20:58 +0900

    Todoアプリに完了機能を追加

--- src/App.jsx
+++ src/App.jsx
@@ -4,40 +4,68 @@ function App() {
   const [text, setText] = useState("");
   const [todos, setTodos] = useState([]);
   const addTodo = () => {
-    if (text.trimRight() === "") {
-      return;
-    }
-    setTodos([...todos, text]);
+    const newText = text.trim();
+
+    if (newText === "") return;
+    setTodos([...todos, {
+      text: newText,
+      completed: false,
+    },
+    ]);
     setText("");
   };
   const deleteTodo = (index) => {
     setTodos(todos.filter((todo, i) => i !== index));
   };
 
+  //   i → 今見ているTodoの番号
+  // index → クリックされたTodoの番号
+  const toggleTodo = (index) => {
+    setTodos(
+      todos.map((todo, i) => {
+        if (i === index) {
+          return {
+            ...todo,
+            completed: !todo.completed,
+          };
+        }
+
+        return todo;
+      })
+    );
+  };
+
   return (
     <>
       <h1> Todoアプリ</h1>
+      {/* Enterキーでも追加できるように */}
       <input
-        // だからReactでは通常、valueを指定してStateで入力欄を管理します。
+        // Reactでは通常、valueを指定してStateで入力欄を管理します。
         type="text"
         value={text}
         onChange={(e) => setText(e.target.value)}
+        onKeyDown={(e) => {
+          if (e.key === "Enter") {
+            addTodo();
+          }
+        }}
       />
       <button onClick={addTodo}>追加</button>
       <p>入力中： {text}</p>
       {todos.map((todo, index) => (
         <div key={index}>
-          <p>{todo}</p>
+          <p style={{
+            textDecoration: todo.completed ? "line-through" : "none",
+          }}>{todo.text}</p>
+          {/* クリックされたときに deleteTodo(index) を実行する関数を渡している */}
           <button onClick={() => deleteTodo(index)}>
             削除
           </button>
+          <button onClick={() => toggleTodo(index)}>完了</button>
         </div>
 
       ))}
-
-
     </>
-
   );
 }
 
